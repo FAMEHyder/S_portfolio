70effada8dd9ad9d5a618fcdfd92155da197ce69
Fame hyder <famehyder9999@gmail.com>
2025-07-25 22:42:26 +0500

    UDB

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -6,18 +6,20 @@ import landscape from '../../public/image/SIB.jpg';
 import logo from '../../public/image/logo.png';
 import ProfilePic from '../../public/image/sib.png'
 
-import landing1 from '../../public/Designes/qq.png';
+import landing1 from '../../public/Designes/bg.png';
 import des1 from '../../public/Designes/aa.png';
-import des2 from '../../public/Designes/asa.png';
-import des3 from '../../public/Designes/kal.jpg';
-import des4 from '../../public/Designes/qq.png';
+import des3 from '../../public/Designes/asa.png';
+import des2 from '../../public/Designes/kal.png';
+import des4 from '../../public/Designes/cg.png';
 import des5 from '../../public/Designes/qq.png';
 
-import bg from '../../public/Designes/bg.png';
+import bg from '../../public/Designes/bg.jpg';
 
 import blog from '../../public/image/12.jpg';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGraduationCap } from '@fortawesome/free-solid-svg-icons'
+import '@fontsource/poppins/700.css'; // Only bold (700 weight)
+
 
 
 const float = {
@@ -62,7 +64,7 @@ const scrollOpenVariant = (index) => ({
     visible: {
         width: '100%',
         opacity: 1,
-        zIndex:1,
+        zIndex: 1,
         transition: {
             duration: 5,
             delay: index * 0,
@@ -338,7 +340,95 @@ export default function Home() {
             </Box>
 
 
+            {/* Gallery section  */}
+            <Box py={10} px={3} sx={{ backgroundColor: '#f9f9f9' }}>
+                <Typography variant="h4" align="center" fontWeight="bold" gutterBottom>
+                    My Best Designs
+                </Typography>
+                <Typography variant="body1" align="center" color="text.secondary" mb={4}>
+                    A selection of my top UI/UX and front-end designs
+                </Typography>
 
+                <Grid >
+                    <Box width={'100%'}>
+                        
+                            <Box
+                                component={'img'}
+                                src={designs[0].image}
+                                width={"90%"}
+                                boxShadow={'2px 2px 2px 2px black'}
+                                sx={{
+                                    ml:{xs:'10px',md:'60px'},
+                                    height:{xs:'100px',md:'300px'},
+                                    transition: 'transform 0.4s ease-in-out',
+                                    '&:hover': {
+                                        transform: 'scale(1.05)', // Slight zoom
+                                        cursor: 'pointer',
+                                    }
+                                }}
+                            />
+                       
+
+
+
+                        {/* this is the main box */}
+
+                    </Box>
+                </Grid>
+
+                {/* This is the next grid  */}
+
+                <Box display="flex" width="100%" justifyContent={'space-between'} mt={10}>
+
+                    {/* Text Section with animation */}
+                    <Box width={'35%'} mt={"12vh"} ml={'40px'}>
+                        <Typography sx={{ fontWeight: 800, fontSize: '30px', fontFamily: 'Poppins, sans-serif', color: '#003296', display: 'flex', justifyContent: 'start', textAlign: 'left', width: '100%' }}>Designed to stand out because average never gets noticed</Typography> <br />
+                        <Typography>
+                            In a digital world full of noise, design is what makes you seen — and remembered.
+                            These social media posts aren’t just pretty visuals; they’re built to engage, connect, and convert.
+                            From bold typography to strategic color use, every element is crafted with purpose.
+                            Because in the world of content, blending in is never an option.
+                        </Typography>
+                    </Box>
+
+
+                    {/* Image Section with animation */}
+                    <motion.div style={{ width: '60%' }} {...pulse}>
+                        <Box height="450px" width="90%" display="flex" sx={{ ml: '10px' }}>
+
+                            {/* Subbox one */}
+                            <Grid height="100%" width="60%" display="flex" flexDirection="column" justifyContent="space-around">
+                                <Box
+                                    height="49%"
+                                    width="98%"
+                                    component="img"
+                                    src={designs[1].image}
+                                    sx={{ objectFit: 'contained', borderRadius: 2 }}
+                                />
+                                <Box
+                                    height="49%"
+                                    width="98%"
+                                    component="img"
+                                    src={designs[2].image}
+                                    sx={{ objectFit: 'contained', borderRadius: 2 }}
+                                />
+                            </Grid>
+
+                            {/* Subbox two */}
+                            <Grid height="100%" width="100%" display="flex" justifyContent="space-around">
+                                <Box
+                                    height="100%"
+                                    width="100%"
+                                    component="img"
+                                    src={designs[3].image}
+                                    sx={{ objectFit: 'contained', borderRadius: 2 }}
+                                />
+                            </Grid>
+                        </Box>
+                    </motion.div>
+                </Box>
+
+            </Box>
 
             {/* skills section  */}
 
@@ -398,9 +488,8 @@ export default function Home() {
             </>
 
 
-
             {/* Experiance section  */}
-            
+
             <Box component="section" py={6} px={{ xs: 2, md: 6 }} bgcolor="#f8f9fa">
                 <Typography variant="h4" fontWeight="bold" gutterBottom sx={{ textAlign: 'center' }} >
                     Graphic Design Experience
@@ -412,7 +501,7 @@ export default function Home() {
                             xs={12}
                             md={6}
                             key={index}
-                            sx={{ p: 3, borderRadius: 2 ,width:'100%'}}
+                            sx={{ p: 3, borderRadius: 2, width: '100%' }}
                         >
                             <motion.div
                                 initial="hidden"
@@ -453,8 +542,8 @@ export default function Home() {
 
             {/* Education section  */}
 
-            <Box id="education" 
-            sx={{ py: 6, px: { xs: 2, md: 6 }, backgroundImage: `url(${bg})`,backgroundSize:'cover',  backgroundPosition: 'center top -100px', width: '96%', ml: 3, borderRadius: '6px' }}
+            <Box id="education"
+                sx={{ py: 6, px: { xs: 2, md: 6 }, backgroundImage: `url(${bg})`, backgroundSize: 'cover', backgroundPosition: 'center top -100px', width: '96%', ml: 3, borderRadius: '6px' }}
             >
                 <Typography variant="h4" align="center" gutterBottom fontWeight={700} color="white">
                     {<FontAwesomeIcon icon={faGraduationCap} size="1x" color="white" />} My Education
@@ -462,101 +551,15 @@ export default function Home() {
                 {/* main grid  */}
                 <Grid height={'300px'} display={'flex'}>
                     <Grid width={'50%'} display={'flex'} flexDirection={'column'}>
-                        <Box width={'100%'} height={'100px'}  > {<FontAwesomeIcon icon={faGraduationCap} size="2x" color="white" />} <Typography color={'white'} sx={{ ml: '40px' }}> <strong>Becholar of Science</strong> <br />Boys Degrre College Skardu <br /> 2023-present</Typography></Box>
-                        <Box width={'100%'} height={'100px'}> {<FontAwesomeIcon icon={faGraduationCap} size="2x" color="white" />} <Typography color={'white'} sx={{ ml: '40px' }}> <strong>FA </strong> <br />Boys Degrre College Skardu <br /> 2021-2023</Typography></Box>
-                        <Box width={'100%'} height={'100px'}> {<FontAwesomeIcon icon={faGraduationCap} size="2x" color="white" />} <Typography color={'white'} sx={{ ml: '40px' }}> <strong>Metriculation</strong> <br />City public school skardu <br /> 2019-2021</Typography></Box>
+                        <Box width={'100%'} height={'100px'} display={'flex'}> {<FontAwesomeIcon icon={faGraduationCap} size="2x" color="white" />} <Typography color={'white'} sx={{ ml: '10px' }}> <strong>Becholar of Science</strong> <br />Boys Degrre College Skardu <br /> 2023-present</Typography></Box>
+                        <Box width={'100%'} height={'100px'} display={'flex'}> {<FontAwesomeIcon icon={faGraduationCap} size="2x" color="white" />} <Typography color={'white'} sx={{ ml: '10px' }}> <strong>FA </strong> <br />Boys Degrre College Skardu <br /> 2021-2023</Typography></Box>
+                        <Box width={'100%'} height={'100px'} display={'flex'}> {<FontAwesomeIcon icon={faGraduationCap} size="2x" color="white" />} <Typography color={'white'} sx={{ ml: '10px' }}> <strong>Metriculation</strong> <br />City public school skardu <br /> 2019-2021</Typography></Box>
                     </Grid>
 
-                    {/* <Grid width={'50%'}
-                        component={'img'}
-                        src={pogo}
-                    ></Grid> */}
                 </Grid>
             </Box>
 
-            {/* Gallery section  */}
-            <Box py={10} px={3} sx={{ backgroundColor: '#f9f9f9' }}>
-                <Typography variant="h4" align="center" fontWeight="bold" gutterBottom>
-                    My Best Designs
-                </Typography>
-                <Typography variant="body1" align="center" color="text.secondary" mb={4}>
-                    A selection of my top UI/UX and front-end designs
-                </Typography>
 
-                <Grid >
-                    <Box display={'flex'} width={'100%'}>
-                        <Box
-                            component={'img'}
-                            src={designs[0].image}
-                            height={'300px'}
-                            width={"100%"}
-                        />
-                      
-
-                        {/* this is the main box */}
-
-                    </Box>
-                </Grid>
-
-                {/* This is the next grid  */}
-                <Grid mt={10} ml={2}>
-                    <Box display="flex" width="100%">
-
-                        {/* Text Section with animation */}
-                        <motion.div style={{ width: '40%' }} {...slideInLoop}>
-                            <Typography>
-                                fugiat aliquam voluptates quod, mollitia maiores! Impedit esse rerum quibusdam nisi.
-                                Id perferendis labore non sint nisi minima ut optio eaque voluptate consequatur quas
-                                fugit fuga deleniti nemo corporis aut, tempore, magnam unde rerum quibusdam! Quae
-                                reiciendis placeat, explicabo ex id assumenda tenetur. Ut labore eius ratione
-                                consectetur culpa aliquam. Fuga labore veritatis, architecto placeat eveniet recusandae
-                                inventore enim suscipit esse unde illum temporibus consequatur nam exercitationem
-                                possimus! Debitis, modi. Nisi similique eum officia! Nobis vero totam possimus veniam
-                                voluptatum, enim ab dolore alias! In, error quis veniam sed officia soluta commodi qui
-                                beatae dolor delectus ad modi impedit odio similique quod ea corrupti totam nemo
-                                dolorum expedita minus consequatur? Aliquam, perspiciatis. Exercitationem aspernatur
-                                rem, architecto officiis debitis officia saepe nulla explicabo consectetur iure, facilis
-                                hic modi temporibus nemo!
-                            </Typography>
-                        </motion.div>
-
-                        {/* Image Section with animation */}
-                        <motion.div style={{ width: '60%' }} {...slideInLoop}>
-                            <Box height="500px" width="100%" display="flex">
-
-                                {/* Subbox one */}
-                                <Grid height="100%" width="60%" display="flex" flexDirection="column" justifyContent="space-around">
-                                    <Box
-                                        height="49%"
-                                        width="98%"
-                                        component="img"
-                                        src={designs[1].image}
-                                        sx={{ objectFit: 'cover', borderRadius: 2 }}
-                                    />
-                                    <Box
-                                        height="49%"
-                                        width="98%"
-                                        component="img"
-                                        src={designs[2].image}
-                                        sx={{ objectFit: 'cover', borderRadius: 2 }}
-                                    />
-                                </Grid>
-
-                                {/* Subbox two */}
-                                <Grid height="100%" width="100%" display="flex" justifyContent="space-around">
-                                    <Box
-                                        height="100%"
-                                        width="100%"
-                                        component="img"
-                                        src={designs[3].image}
-                                        sx={{ objectFit: 'cover', borderRadius: 2 }}
-                                    />
-                                </Grid>
-                            </Box>
-                        </motion.div>
-                    </Box>
-                </Grid>
-            </Box>
         </Box>
     );
 }
